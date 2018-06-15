9de1892ccd11d53c115529f62585f851a8561c2e
gudbrandtandberg <gudbrandduff@gmail.com>
2018-06-14 21:42:25 -0700

    merged refactor into master

---
+++ computeroot/cleanup_uploads_folder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm user_uploads/failboards/*
+rm user_uploads/labelled/*/*
+rm user_uploads/raw_success/*
+rm user_uploads/raw_fail/*
+rm user_uploads/unlabelled/*/*
---
+++ webroot/static/js/main.js
@@ -0,0 +1,96 @@
+var init = function() {
+        
+    var board = ChessBoard('board', 'start');
+    
+    $("form#theform").submit(function(event) {
+        event.preventDefault()
+        var file = document.getElementById("imageinput").files[0]
+        // TODO input check. 
+        var formData = new FormData(this);
+        
+        $.ajax({
+            url: "http://localhost:7777/cv_algo/",
+            method: "POST", 
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                //parse data = {FEN: "...", id: "..."}
+                res = JSON.parse(data)
+                
+                if (res.error == "false") {
+                    setFEN(res.FEN)
+                    //$("#feedback_pane").css("visibility", "visible")
+                    document.getElementById("raw-id-input").value = res.id
+                    //document.getElementById("feedback_pane").style.visibility = "visible"
+                } else {
+                    alert(res.errMsg)
+                }
+            },
+            error: function(xmlHttpRequest, textStatus, errorThrown) {
+                if(xmlHttpRequest.readyState == 0 || xmlHttpRequest.status == 0) {
+                    alert("prematurely aborting ajax request..")
+                    return
+                } else {
+                    alert(textStatus)
+                }
+            }
+        })
+    })
+
+    $("form#feedback_form").submit(function(event) {
+    
+        event.preventDefault()
+        var formData = new FormData(this);
+    
+        $.ajax({
+            url: "http://localhost:7777/feedback/",
+            method: "POST",
+            data: formData, 
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                res = JSON.parse(data)
+                console.log(res)
+                if (res.success == "true") {
+                    alert("Thanks for your feedback!")
+                } else {
+                    alert("Your feedback was not taken into consideration")
+                    }              
+                },
+            error: function(data) {
+                alert(data)
+                }
+            })
+        })
+    document.getElementById('imageinput').onchange = function (evt) {
+        var tgt = evt.target || window.event.srcElement,
+            files = tgt.files;
+
+        // FileReader support
+        if (FileReader && files && files.length) {
+            var fr = new FileReader();
+            fr.onload = function(data) {
+                var imageData = data.target.result
+                // Show preview of uploaded image
+                document.getElementById("inputpreview").src = imageData
+            }
+            fr.readAsDataURL(files[0]);
+        }
+
+        // Not supported
+        else {
+            // fallback -- perhaps submit the input to an iframe and temporarily store
+            // them on the server until the user's session ends.
+        }
+    }
+
+    }; // end init()
+
+var setFEN = function(fen) {
+    var board = ChessBoard('board', fen);
+}
+
+$(document).ready(init);
