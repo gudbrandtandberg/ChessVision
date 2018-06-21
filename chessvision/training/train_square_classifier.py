13fc8996cd4c1385de57e689fe16d8983365c803
gudbrandtandberg <gudbrandduff@gmail.com>
2018-06-21 16:13:48 -0700

    new data and architecture

--- chessvision/training/train_square_classifier.py
+++ chessvision/training/train_square_classifier.py
@@ -11,12 +11,27 @@ epochs = 12
 
 train_datagen = ImageDataGenerator(
         rescale=1./255,
+        samplewise_center=False,
+        rotation_range=10,
+        zoom_range=0.05,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        shear_range=5
         )
 
-#train_datagen.fit()
+valid_datagen = ImageDataGenerator(
+        rescale=1./255
+        )
 
 train_generator = train_datagen.flow_from_directory(
-        '../data/squares/',
+        '../data/squares/training/',
+        target_size=cv_globals.PIECE_SIZE,
+        color_mode='grayscale',
+        batch_size=32,
+        class_mode='categorical')
+
+valid_generator = valid_datagen.flow_from_directory(
+        '../data/squares/validation/',
         target_size=cv_globals.PIECE_SIZE,
         color_mode='grayscale',
         batch_size=32,
@@ -33,23 +48,27 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-callbacks = [EarlyStopping(monitor='loss',
+callbacks = [EarlyStopping(monitor='val_loss',
                            patience=8,
                            verbose=1,
                            min_delta=1e-4),
-             ReduceLROnPlateau(monitor='loss',
+             ReduceLROnPlateau(monitor='val_loss',
                                factor=0.1,
                                patience=4,
                                verbose=1,
                                min_delta=1e-4),
-             ModelCheckpoint(monitor='loss',
+             ModelCheckpoint(monitor='val_loss',
                              filepath='../weights/best_weights_square_gen4.hdf5',
                              save_best_only=True,
                              save_weights_only=True),
              TensorBoard(log_dir=cv_globals.CVROOT + 'logs/square_logs/')]
 
 model.fit_generator(generator=train_generator,
-                    steps_per_epoch=np.ceil(6266./batch_size),
+                    steps_per_epoch=np.ceil(4975./batch_size),
                     epochs=100,
                     verbose=1,
-                    callbacks=callbacks)
+                    callbacks=callbacks,
+                    validation_data=valid_generator,
+                    validation_steps=np.ceil(1291./batch_size))
+
+# use class_weights!
