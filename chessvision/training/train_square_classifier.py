84d0446236fbdd8b7c1f1d2ca1e5109f0cce016b
gudbrandtandberg <gudbrandduff@gmail.com>
2018-06-21 17:59:36 -0700

    new square model

--- chessvision/training/train_square_classifier.py
+++ chessvision/training/train_square_classifier.py
@@ -9,40 +9,40 @@ num_classes = 13
 batch_size = 32
 epochs = 12
 
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        samplewise_center=False,
-        rotation_range=10,
-        zoom_range=0.05,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        shear_range=5
-        )
-
-valid_datagen = ImageDataGenerator(
-        rescale=1./255
-        )
-
-train_generator = train_datagen.flow_from_directory(
-        '../data/squares/training/',
-        target_size=cv_globals.PIECE_SIZE,
-        color_mode='grayscale',
-        batch_size=32,
-        class_mode='categorical')
-
-valid_generator = valid_datagen.flow_from_directory(
+def get_validation_generator():
+        valid_datagen = ImageDataGenerator(
+                rescale=1./255
+                )
+        valid_generator = valid_datagen.flow_from_directory(
         '../data/squares/validation/',
         target_size=cv_globals.PIECE_SIZE,
         color_mode='grayscale',
         batch_size=32,
         class_mode='categorical')
+        return valid_generator
+
+def get_train_generator():
+        train_datagen = ImageDataGenerator(
+                rescale=1./255,
+                samplewise_center=False,
+                rotation_range=10,
+                zoom_range=0.05,
+                width_shift_range=0.1,
+                height_shift_range=0.1,
+                shear_range=5
+                )
+
+        train_generator = train_datagen.flow_from_directory(
+                '../data/squares/training/',
+                target_size=cv_globals.PIECE_SIZE,
+                color_mode='grayscale',
+                batch_size=32,
+                class_mode='categorical')
+        return train_generator
 
 # Build the model
 
 model = build_square_classifier()
-
-#from keras.utils import plot_model
-#plot_model(model, to_file='model.png')
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
@@ -56,19 +56,21 @@ callbacks = [EarlyStopping(monitor='val_loss',
                                factor=0.1,
                                patience=4,
                                verbose=1,
-                               min_delta=1e-4),
+                               epsilon=1e-4),
              ModelCheckpoint(monitor='val_loss',
-                             filepath='../weights/best_weights_square_gen4.hdf5',
+                             filepath='../weights/best_weights_square_new.hdf5',
                              save_best_only=True,
                              save_weights_only=True),
              TensorBoard(log_dir=cv_globals.CVROOT + 'logs/square_logs/')]
 
-model.fit_generator(generator=train_generator,
+print(model.summary())
+
+model.fit_generator(generator=get_train_generator(),
                     steps_per_epoch=np.ceil(4975./batch_size),
                     epochs=100,
                     verbose=1,
                     callbacks=callbacks,
-                    validation_data=valid_generator,
+                    validation_data=get_validation_generator(),
                     validation_steps=np.ceil(1291./batch_size))
 
 # use class_weights!
