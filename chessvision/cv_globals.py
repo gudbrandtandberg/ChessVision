ffccff9613a9a9ed49813d7ce453a44fed3ebc66
gudbrandtandberg <gudbrandduff@gmail.com>
2018-07-20 12:07:19 +0200

    removed logs and halfheartedly started new coord-model

--- chessvision/cv_globals.py
+++ chessvision/cv_globals.py
@@ -7,8 +7,8 @@ compute_root = os.path.join(CVROOT, "computeroot/")
 squares_train_dir = os.path.join(CVROOT, "data/squares/training/")
 squares_validation_dir = os.path.join(CVROOT, "data/squares/validation/")
 
-image_dir = os.path.join(CVROOT, "/data/board_extraction/images/")
-mask_dir = os.path.join(CVROOT, "/data/board_extraction/masks/")
+image_dir = os.path.join(CVROOT, "data/board_extraction/images/")
+mask_dir = os.path.join(CVROOT, "data/board_extraction/masks/")
 
 square_weights = os.path.join(CVROOT, "weights/best_weights_square.hdf5")
 square_weights_train = os.path.join(CVROOT, "weights/best_weights_square_new.hdf5")
