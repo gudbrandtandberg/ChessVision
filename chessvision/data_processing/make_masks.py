b650709f335a523c882dc1d5105c60aa4e22c071
gudbrandtandberg <gudbrandduff@gmail.com>
2018-07-25 23:49:36 +0200

    more segmentation data and deleted weights from repo

--- chessvision/data_processing/make_masks.py
+++ chessvision/data_processing/make_masks.py
@@ -5,7 +5,7 @@ from util import listdir_nohidden
 import cv2
 #import matplotlib.pyplot as plt
 import os
-import os.path.join as join
+from os.path import join
 import cv_globals
 
 
@@ -27,17 +27,18 @@ def make_masks(input_dir, output_dir, image_dir, json_file):
             
             mask = np.zeros((w, h , 1), np.uint8)
             cv2.fillConvexPoly(mask, points, 255)
-            cv2.imwrite(join(output_dir, filename), mask)
             print("Making mask for {}".format(filename))
-            os.rename(join(input_dir, filename), join(image_dir, filename))
+            cv2.imwrite(join(output_dir, filename), mask)
+            
+            #os.rename(join(input_dir, filename), join(image_dir, filename))
 
 if __name__ == "__main__":
 
     #python chessvision/data_processing/make_masks.py
 
     json_file = join(cv_globals.data_root, "board_extraction/coordinates.json")
-    input_dir = "data/board_extraction/new_raw_resized/"
-    output_dir = "data/board_extraction/masks/"
-    image_dir = "data/board_extraction/images/"
+    input_dir = join(cv_globals.data_root, "new_raw_resized/")
+    output_dir = join(cv_globals.data_root, "board_extraction/masks/")
+    image_dir = join(cv_globals.data_root, "data/board_extraction/images/")
 
     make_masks(input_dir, output_dir, image_dir, json_file)
