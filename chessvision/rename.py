ec97b74f7304ddbb58fc81290b7e5e7c6983ce84
gudbrandtandberg <gudbrandduff@gmail.com>
2018-11-24 18:20:51 +0100

    renamed squares

--- chessvision/rename.py
+++ chessvision/rename.py
@@ -6,17 +6,21 @@ import uuid
 i = 0
 bases = [cv_globals.data_root + "squares/validation", cv_globals.data_root + "squares/training"]
 
+tally = {}
 for base in bases:
     for dirname in listdir_nohidden(base):
         for fname in listdir_nohidden(base + "/" + dirname):
-            if len(fname) > 150:
+            if len(fname) not in tally:
+                tally[len(fname)] = 0 
+            else:   
+                tally[len(fname)] += 1
+            if len(fname) != 18:
                 src = base + "/" + dirname + "/" + fname
                 uid = str(uuid.uuid1())
                 dst = os.path.join(base, dirname, uid + ".JPG")
                 #print("saving long file: {}".format(dst))
                 os.rename(src, dst)
-                
-                i += 1
+            
 
-print(i)
+print(tally)
 
