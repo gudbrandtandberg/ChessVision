bbfb6769f2428d456ff9ed409f9ccd5930774e12
gudbrandtandberg <gudbrandduff@gmail.com>
2018-11-29 13:37:57 +0100

    Add basic logging to cv_endpoint

--- computeroot/cv_endpoint.py
+++ computeroot/cv_endpoint.py
@@ -20,14 +20,29 @@ from u_net import load_extractor
 from square_classifier import load_classifier
 from util import BoardExtractionError
 from flask.logging import default_handler
+import logging 
 
 app = Flask(__name__)
-#app.logger.removeHandler(default_handler)
-#from logging.config import dictConfig
-#import logging
-#import logging.config
 
-#logging.config.fileConfig("cv.log")
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        record.url = request.url
+        record.remote_addr = request.remote_addr
+        return super().format(record)
+
+formatter = RequestFormatter(
+    "[%(asctime)s] %(remote_addr)s requested %(url)s. "
+    "%(levelname)s in %(name)s: %(message)s"
+)
+
+logger = logging.getLogger("chessvision")
+file_handler = logging.FileHandler('cv_endpoint.log', 'w')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
                 attach_to_all=True, automatic_options=True):
@@ -95,7 +110,7 @@ def allowed_file(filename):
 @app.route('/cv_algo/', methods=['POST'])
 @crossdomain(origin='*')
 def predict_img():
-    app.logger.info("CV-Algo invoked")
+    logger.info("CV-Algo invoked")
     
     #Host, User-Agent, Content-Length, Origin
     image = read_image_from_formdata()
@@ -117,6 +132,7 @@ def predict_img():
             tomove = request.form["tomove"]
 
         try:
+            logger.info("Processing image {}".format(filename))
             board_img, _, _, _, FEN, _ = classify_raw(image, filename, board_model, sq_model, flip=flip)
             #move file to success raw folder
             os.rename(tmp_loc, os.path.join(app.config["UPLOAD_FOLDER"], "raw", filename))
@@ -153,7 +169,7 @@ def receive_feedback():
     res = '{"success": "false"}'
 
     if "id" not in request.form or "position" not in request.form or "flip" not in request.form:
-        print("Missing form data, abort!")
+        logger.error("Missing form data, abort!")
         return res
 
     raw_id = request.form['id']
@@ -205,7 +221,7 @@ def analyze(fen, move):
     elif plat == "Darwin":
         sf_binary = "./stockfish-9-64-mac"
     else:
-        print("No support for windows..")
+        logger.error("No support for windows..")
         return res
 
     stockfish = Engine(sf_binary, depth=10)
@@ -213,11 +229,11 @@ def analyze(fen, move):
     try: 
         stockfish.setposition(fen)
     except Exception as e:
-        print("BOARD ILLEGAL!: {}".format(e))
+        logger.error("BOARD ILLEGAL!: {}".format(e))
         return res
     try:
         best_move = stockfish.bestmove()
-        print("Best move is: {}".format(best_move["bestmove"])) #is '(none)' if there is mate
+        logger.debug("Best move is: {}".format(best_move["bestmove"])) #is '(none)' if there is mate
         info = best_move["info"].split()
 
         if "cp" in info:
@@ -230,7 +246,7 @@ def analyze(fen, move):
             mate = int(info[info.index("mate")+1])
     
     except Exception as e:
-        print("STOCKFISH FAILED: {}".format(e))
+        logger.error("STOCKFISH FAILED: {}".format(e))
         return res
     
     return {"best_move": best_move["bestmove"], "score": score, "mate": mate}
@@ -238,11 +254,11 @@ def analyze(fen, move):
 @app.route('/analyze/', methods=['POST'])
 @crossdomain(origin='*')
 def engine_analyze():
-    print("Analyzing position using Stockfish")
+    logger.info("Analyzing position using Stockfish")
     res = '{{ "success": "false" }}'
 
     if "FEN" not in request.form:
-        print("No FEN in the form data...")
+        logger.error("No FEN in the form data...")
         return res
 
     fen = request.form["FEN"]
@@ -266,7 +282,7 @@ def read_image_from_formdata():
     # check if the post request has the file part
     
     if 'file' not in request.files:
-        print("No file")
+        logger.error("No file")
         return None
 
     file = request.files['file']
@@ -282,16 +298,17 @@ def load_models():
     #global sq_model, board_model
 
     sq_model = load_classifier(weights=cv_globals.square_weights)
-    board_model = load_extractor()
+    board_model = load_extractor(weights=cv_globals.board_weights)
 
     return board_model, sq_model
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--local", type=bool, default=False)
+    parser.add_argument("--local", action="store_true")
     args = parser.parse_args()
-    
-    port = 8080 if args.local else 7777
+
+    port = 7777 if args.local else 8080
     board_model, sq_model = load_models()
+    
     app.run(host='0.0.0.0', port=port)
