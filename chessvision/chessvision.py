33c24dc0eee278d1fc4ae9f4af00b666f7829029
gudbrandtandberg <gudbrandduff@gmail.com>
2018-11-29 13:37:46 +0100

    Small cleanup

--- chessvision/chessvision.py
+++ chessvision/chessvision.py
@@ -9,13 +9,11 @@ import board_classifier
 
 def classify_raw(img, filename, board_model, sq_model, flip=False, threshold=80):
 
-    #filename = path.split("/")[-1]
     print("Processing image {}".format(filename))
     
     ###############################   STEP 1    #########################################
 
     ## Resize image
-    #img = cv2.imread(path)
     comp_image = cv2.resize(img, cv_globals.INPUT_SIZE, interpolation=cv2.INTER_AREA)
     
     ## Extract board using CNN model and contour approximation
@@ -26,7 +24,7 @@ def classify_raw(img, filename, board_model, sq_model, flip=False, threshold=80)
     ###############################   STEP 2    #########################################
     
     FEN, predictions, chessboard, squares = board_classifier.classify_board(board_img, sq_model, flip=flip)
-    #del sq_model
+
     #print("Processing image {}.. DONE".format(filename))
     
     return board_img, mask, predictions, chessboard, FEN, squares
