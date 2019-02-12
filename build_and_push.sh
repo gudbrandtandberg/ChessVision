6df3c6c4aae82a38a6d09bed8e9a0ff442b949ef
gudbrandtandberg <gudbrandduff@gmail.com>
2019-02-12 12:00:43 +0100

    Finish refactoring for now

--- build_and_push.sh
+++ build_and_push.sh
@@ -13,7 +13,7 @@ then
     exit 1
 fi
 
-chmod +x src/serve
+chmod +x container/serve
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
