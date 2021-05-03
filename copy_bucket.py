472d8460bf00d3da079469d2653f61e40e0be36e
Gudbrand Tandberg <gudbrand.tandberg@bluware.com>
2021-05-03 15:19:03 +0200

    Make copy bucket work on windows

--- copy_bucket.py
+++ copy_bucket.py
@@ -17,8 +17,9 @@ for dirName, subdirList, fileList in os.walk(rootDir):
         contentType = "text/plain"
         if fname[0] == "." or fname in avoid:
             continue
-        if fname == "index.html" and dirName == "webroot/templates":
-            continue
+        if fname == "index.html":
+            if "webroot" in dirName and "templates" in dirName:
+                continue
         if fname.endswith(".html"):
             contentType = "text/html"
         if fname.lower().endswith(".jpg"):
@@ -32,7 +33,7 @@ for dirName, subdirList, fileList in os.walk(rootDir):
         if fname.endswith(".ico"):
             contentType = "image/x-icon"
         
-        key = "{}/{}".format(dirName, fname).replace("webroot/", "")
+        key = os.path.join(dirName, fname).replace("\\", "/").replace("webroot/", "")
         print(key)
         f = os.path.join(dirName, fname)
         with open (f, "rb") as fHandle:
