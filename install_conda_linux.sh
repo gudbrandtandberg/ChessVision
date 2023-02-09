1f85e0ddff5a555b0371dfeb68e62c57d2eaec15
Gudbrand Tandberg <gudbrand.tandberg@bluware.com>
2023-02-09 11:49:40 +0100

    Delete unused files

--- install_conda_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-cd /tmp
-curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash Anaconda3-5.0.1-Linux-x86_64.sh
-source ~/.bashrc
-conda list
--- install_pip_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-sudo apt-get update
-sudo apt-get install python-pip python-dev build-essential
-sudo pip install --upgrade pip
-sudo pip install --upgrade virtualenv
-
