71d818f13405e26b0b8c06cd2fd948a1162cb02d
Gudbrand Tandberg <gudbrand.tandberg@bluware.com>
2023-02-12 20:37:44 +0100

    UPdate

--- local_test/predict_aws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CHESSVISION_ENDPOINT = "https://4e4q3rd1b5.execute-api.eu-central-1.amazonaws.com/default/chessvisionClient"
-
-curl -X POST \
--H "Content-Type: application/json" \
--d @test.json $CHESSVISION_ENDPOINT
-
