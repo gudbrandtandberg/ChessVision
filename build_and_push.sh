91751ffcae637322297e05cdd9e7dda548c07970
Gudbrand Tandberg <gudbrand.tandberg@bluware.com>
2023-02-14 14:53:19 +0100

    Data updates

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,13 +1,13 @@
 
 image="chessvision-algo"
 version="1.0"
-region=eu-central-1
+region=$(aws configure get region --profile personal)
 account=$(aws sts get-caller-identity --query Account --output text)
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:${version}"
 
 # Get the login command from ECR and execute it directly
-# $(aws ecr get-login --region ${region} --no-include-email)
-#aws ecr get-login-password --region eu-central-1 --profile personal | docker login --username AWS --password-stdin 580857158266.dkr.ecr.eu-central-1.amazonaws.com
+aws ecr get-login-password --region eu-central-1 --profile personal | docker login --username AWS --password-stdin 580857158266.dkr.ecr.eu-central-1.amazonaws.com
+# $(aws ecr get-login-password --profile personal) | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
 # 580857158266.dkr.ecr.eu-central-1.amazonaws.com/chessvision-algo
 
 # Build the docker image locally with the image name and then push it to ECR
